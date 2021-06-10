c90e9b4c4629997041df803ff8fea9badad9c5ec
André Cabral <andrecabrall@gmail.com>
2021-06-10 08:03:11 -0300

    Primeiro Commir

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/content">Content</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  mounted: function() {
+    this.verificaLogin();
+  },
+
+  methods: {
+    verificaLogin() {
+     
+      this.$store.dispatch("verificaToken", window.localStorage.token);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/Content.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<h1>Conteudo Liberado</h1>
+<h2>Bem vindo {{$store.state.user}}</h2>
+        
+      
+    </div>
+</template>
+
+
+<script>
+
+ 
+export default {
+    name: "Content",
+
+}
+</script>
